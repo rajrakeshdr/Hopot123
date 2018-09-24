5664f592bd75c63343fd5cdd49d1b1764afe885e
rajrakeshdr <raj.rakeshdr@gmail.com>
2018-09-24 18:03:45 +0530

    Add files via upload

---
+++ ftp_logging.py
@@ -0,0 +1,31 @@
+import datetime
+import os
+
+#if the file is not created create IT
+
+LOG_LOCATION = '/opt/HoneyFTP/logs/ftp.log'
+f1 = open(LOG_LOCATION,'a+')
+
+Lines=f1.readlines()
+searchquery = '@'
+
+for i in range(0,len(Lines)):
+	if searchquery in Lines[i]:
+		st=Lines[i].index('@')
+		ed=Lines[i].index(' ')
+		ip=Lines[i][st+1:ed]
+		dir="./logs/"+ip
+		if not os.path.exists(dir):
+			os.makedirs(dir)
+
+		x = datetime.datetime.now()
+		dt=str(x.day)+'-'+str(x.month)+'-'+str(x.year)
+		addr=dir+'/'+dt+'.log'
+		f2=open(addr,'a+')
+		f2.write(Lines[i])
+		f2.close()
+
+f1.close()
+#This will clean the file to start relogging
+f1 = open(LOG_LOCATION,'w')
+f1.close()
---
+++ ftp_server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import os,sys
+import logging
+import ConfigParser
+from random import randint
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+BASE_LOCATION = "/opt/HoneyFTP/honeyPotServices/"
+FTP_DIRECTORY = BASE_LOCATION + "ftpDrive"
+
+if __name__ == "__main__":
+
+	authorizer = DummyAuthorizer()
+	config = ConfigParser.RawConfigParser()
+	config.read(BASE_LOCATION + 'config.cfg')
+
+   	usernames =  [i.strip() for i in config.get('users','usernames').split(',')]
+   	passwords = [i.strip() for i in config.get('users','password').split(',')]
+
+   	if len(usernames) != len(passwords):
+   		print "User name Password length Mismatch ... Quitting"
+   		sys.exit(0);
+
+   	for i in range(len(usernames)):
+   		authorizer.add_user(usernames[i],passwords[i],FTP_DIRECTORY,perm='elradfmwM')
+
+	authorizer.add_anonymous(os.getcwd())
+	handler = FTPHandler
+	handler.authorizer = authorizer
+
+	f=open(BASE_LOCATION + 'banners/banner'+str(randint(0,5))+'.dat')
+
+	s=""
+	for line in f:
+		s=s+line
+
+	f.close()
+
+	handler.banner = s
+
+	logging.basicConfig(filename=BASE_LOCATION+'logs/ftp.log', level=logging.DEBUG)
+	handler.log_prefix = '[%(username)s]@%(remote_ip)s'
+	address = ('',21)
+	server = FTPServer(address, handler)
+	server.max_cons = 256
+	server.max_cons_per_ip = 1
+
+	server.serve_forever()
